601efd8fd315f1b307260d3336399f9f8d62d6f1
Arthur Schwaiger <arthur@schwaiger-fam.net>
2025-11-13 13:06:43 +0100

    test build action on commit

--- src/main/java/io/github/atur256/graalvmwebimageinterop/Main.java
+++ src/main/java/io/github/atur256/graalvmwebimageinterop/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     // TODO: temporary
 
+    // TODO: Test
 
     public static void main(String[] args) {
         System.out.println("Starting WebImage interop test suite...\n");
